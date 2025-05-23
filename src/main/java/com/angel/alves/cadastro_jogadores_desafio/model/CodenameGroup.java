fa1e294ba2139323bf0ef1ce6d36122decd2f975
angelalvess <angeltrashangel@gmail.com>
2025-05-23 13:52:02 -0300

    feat: public Jogador withCodinome(String codinome) {

---
+++ src/main/java/com/angel/alves/cadastro_jogadores_desafio/model/CodenameGroup.java
@@ -0,0 +1,23 @@
+package com.angel.alves.cadastro_jogadores_desafio.model;
+
+public enum CodenameGroup {
+
+    VINGADORES("Os Vingadores", "https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/vingadores.json"),
+    LIGA_DA_JUSTICA("A Liga da Justiça","https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml");
+
+    private String name;
+    private String uri;
+
+    CodenameGroup(String name, String uri) {
+        this.name = name;
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Jogadores (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20),
+    telephone VARCHAR(20),
     codinome VARCHAR(255),
     grupo_codinome VARCHAR(255) NOT NULL,
     CONSTRAINT unique_codinome_grupo UNIQUE (codinome, grupo_codinome)
