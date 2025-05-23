16ebc6eede46b9a77da9513e577c3e9d1c904119
angelalvess <angeltrashangel@gmail.com>
2025-05-23 13:33:13 -0300

    feat: init project cadastro jogadores desafio

---
+++ src/main/java/com/angel/alves/cadastro_jogadores_desafio/CadastroJogadoresDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.angel.alves.cadastro_jogadores_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadoresDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadoresDesafioApplication.class, args);
+	}
+
+}
